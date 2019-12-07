492a9cb8feafe1017661fa71ac87f437d77f1e8a
root <mehedishafi@hotmail.com>
2019-12-07 12:22:41 +0600

    feat: searching countries

--- app/src/main/java/com/example/shafi/databaseimport/MainActivity.java
+++ app/src/main/java/com/example/shafi/databaseimport/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.shafi.databaseimport;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,14 +21,33 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Country> dbCountries  = new ArrayList<>();
     private CountryListAdapter countryAdapter;
     private Country tempCountry;
+    private EditText searchBox;
+    private Button runSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView = (ListView) findViewById(R.id.mainList);
+        searchBox = (EditText) findViewById(R.id.search_text);
+
         refreshData();
 
+        findViewById(R.id.search_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                search();
+            }
+        });
+    }
+
+    private void search(){
+        String searchString = searchBox.getText().toString().trim();
+        if (null != searchString && searchString.compareToIgnoreCase("") != 0){
+            Intent i = new Intent(MainActivity.this, SearchResult.class);
+            i.putExtra("searchString", searchString);
+            startActivity(i);
+        }
     }
 
 
